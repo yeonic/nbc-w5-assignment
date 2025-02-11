e3e6c1383e28e63fa529b00293a6977b5259dcc4
김종연 <bellopenerr@gmail.com>
2025-02-11 14:41:12 +0900

    initial commit

---
+++ src/main/java/me/yeon/nbcw5assignment/NbcW5AssignmentApplication.java
@@ -0,0 +1,13 @@
+package me.yeon.nbcw5assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcW5AssignmentApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NbcW5AssignmentApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/yeon/nbcw5assignment/NbcW5AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yeon.nbcw5assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcW5AssignmentApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
