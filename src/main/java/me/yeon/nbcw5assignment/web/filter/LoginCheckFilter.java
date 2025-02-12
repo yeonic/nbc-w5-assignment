9cccb7a980d07783ab942501f52161d23b9473e5
김종연 <bellopenerr@gmail.com>
2025-02-12 22:46:22 +0900

    fix: filter exception 위임처리

--- src/main/java/me/yeon/nbcw5assignment/web/filter/LoginCheckFilter.java
+++ src/main/java/me/yeon/nbcw5assignment/web/filter/LoginCheckFilter.java
@@ -6,16 +6,21 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.yeon.nbcw5assignment.web.SessionConst;
 import me.yeon.nbcw5assignment.web.filter.exception.UnauthorizedException;
 import org.springframework.util.PatternMatchUtils;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @Slf4j
+@RequiredArgsConstructor
 public class LoginCheckFilter implements Filter {
 
+  private final HandlerExceptionResolver resolver;
   private static final String[] whitelist = {"/api/login", "/api/logout", "/api/users/signup"};
 
   @Override
@@ -23,12 +28,18 @@ public class LoginCheckFilter implements Filter {
       FilterChain filterChain) throws IOException, ServletException {
 
     HttpServletRequest request = (HttpServletRequest) servletRequest;
+    HttpServletResponse response = (HttpServletResponse) servletResponse;
     String requestURI = request.getRequestURI();
 
     if (isLoginCheckPath(requestURI)) {
       HttpSession session = request.getSession(false);
       if (isUnauthorized(session)) {
-        throw new UnauthorizedException("인증되지 않은 사용자입니다.");
+        try {
+          throw new UnauthorizedException("인증되지 않은 사용자입니다.");
+        } catch (Exception e) {
+          resolver.resolveException(request, response, null, e);
+          return;
+        }
       }
     }
 
