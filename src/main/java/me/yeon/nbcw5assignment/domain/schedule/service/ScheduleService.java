11470cf42d203f66545f8439959b87ee4eb06aa1
김종연 <bellopenerr@gmail.com>
2025-02-13 10:54:13 +0900

    feat: Schedule에 commentCount 표시, 공통 메시지처리

--- src/main/java/me/yeon/nbcw5assignment/domain/schedule/service/ScheduleService.java
+++ src/main/java/me/yeon/nbcw5assignment/domain/schedule/service/ScheduleService.java
@@ -2,6 +2,7 @@ package me.yeon.nbcw5assignment.domain.schedule.service;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import me.yeon.nbcw5assignment.domain.comment.dao.CommentRepository;
 import me.yeon.nbcw5assignment.domain.schedule.Schedule;
 import me.yeon.nbcw5assignment.domain.schedule.dao.ScheduleRepository;
 import me.yeon.nbcw5assignment.domain.schedule.dto.ScheduleDto;
@@ -16,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class ScheduleService {
 
   private final ScheduleRepository repository;
+  private final CommentRepository commentRepository;
 
   public ScheduleDto.Res addSchedule(User author, String title, String content) {
     Schedule schedule = Schedule.builder()
@@ -23,26 +25,32 @@ public class ScheduleService {
         .title(title)
         .content(content)
         .build();
-    return ScheduleMapper.toRes(repository.save(schedule));
+    return ScheduleMapper.toRes(repository.save(schedule), 0L);
   }
 
   public ScheduleDto.Res updateSchedule(Long scheduleId, String title, String content) {
     Schedule schedule = repository.findById(scheduleId).orElseThrow();
-
     String updateTitle = title == null ? schedule.getTitle() : title;
     String updateContent = content == null ? schedule.getContent() : content;
 
-    return ScheduleMapper.toRes(repository.update(scheduleId, updateTitle, updateContent));
+    Schedule updatedSchedule = repository.update(scheduleId, updateTitle, updateContent);
+    Long commentCount = commentRepository.countAllByScheduleId(updatedSchedule.getId());
+
+    return ScheduleMapper.toRes(updatedSchedule, commentCount);
   }
 
   public ScheduleDto.Res getSchedule(Long scheduleId) {
     Schedule schedule = repository.findById(scheduleId).orElseThrow();
-    return ScheduleMapper.toRes(schedule);
+    Long commentCount = commentRepository.countAllByScheduleId(schedule.getId());
+    return ScheduleMapper.toRes(schedule, commentCount);
   }
 
   public List<ScheduleDto.Res> getSchedulesOfUser(Long userId) {
     return repository.findAllByUserId(userId)
-        .stream().map(ScheduleMapper::toRes)
+        .stream().map(schedule -> {
+          Long commentCount = commentRepository.countAllByScheduleId(schedule.getId());
+          return ScheduleMapper.toRes(schedule, commentCount);
+        })
         .toList();
   }
 
